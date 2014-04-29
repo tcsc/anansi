eaf1556527ae9d2212c76e8f33ba298132649396
Trent Clarke <trent.clarke@biarri.com>
2014-04-29 14:40:05 +1000

    Retabbed, added progress info to unit test blocks, added insert

--- source/container.d
+++ source/container.d
@@ -1,5 +1,8 @@
 import std.algorithm, std.conv, std.range, std.stdio, std.traits;
 
+/**
+ *
+ */
 package struct Array(T) {
 public:
     this(U)(U[] stuff) if (isImplicitlyConvertible!(U, T)) {
@@ -23,9 +26,21 @@ public:
 
 public:
     size_t insertBack(T value) {
-        ensureSpace(_size+1);
-        _payload[_size] = move(value);
-        return _size++;
+      ensureSpace(_size+1);
+      _payload[_size] = move(value);
+      return _size++;
+    }
+
+    size_t insert(T value, size_t index) 
+    in {
+      assert(index <= _size);
+    }
+    body {
+      ensureSpace(_size+1);
+      moveAll(_payload[index .. $-1], _payload[index + 1 .. $]);
+      _payload[index] = move(value);
+      _size += 1;
+      return index;
     }
 
     void erase(size_t index) {
@@ -101,144 +116,170 @@ private:
 }
 
 unittest {
-    // empty construction
-    Array!string s;
-    assert(s.length == 0);
+  writeln("Array: empty construction.");
+  Array!string s;
+  assert(s.length == 0);
+  assert(s.capacity >= 0);
 }
 
 unittest {
-    // construction from an array
-    auto data = ["alpha", "beta", "delta", "gamma", "epsilon"];
-    auto s = Array!string(data);
+  writeln("Array: construction from a Range.");
+  auto data = ["alpha", "beta", "delta", "gamma", "epsilon"];
+  auto s = Array!string(data);
 
-    assert(s.length == data.length);
-    for(auto i = 0; i < data.length; ++i)
-        assert(s[i] == data[i]);
+  assert(s.length == data.length);
+  for(auto i = 0; i < data.length; ++i)
+    assert(s[i] == data[i]);
 }
 
 unittest {
-    // erasing a single item - make sure that all elements have moved up the line.
-    auto a = Array!int(iota(0, 100, 1));
-    assert(a.length == 100);
-    assert(a[7] == 7);
+  writeln("Array: insertion into empty Array");
+  Array!string a;
+  auto i = a.insert("hi", 0);
+  assert (a.length == 1, "Incorrect length after insertion.");
+  assert (a[0] == "hi", "Incorrect value at insertion point.");
+  assert (i == 0, "Incorrect insertion point index returned.");
+}
 
-    a.erase(7);    
-    assert(a.length == 99);
-    foreach(i; 0 .. 7)
-        assert(a[i] == i);
+unittest {
+  writeln("Array: insertion at the end of an Array.");
+  auto data = ["alpha", "beta", "delta", "gamma", "epsilon"];
+  auto a = Array!string(data);
+  auto i = a.insert("o hai", a.length);
+  assert (i == data.length, "Incorrect insertion point index returned.");
 
-    foreach(i; 7 .. 99)
-        assert(a[i] == (i+1));
+  assert (a.length == (data.length+1), "Incorrect length after insertion.");  
+
+  for(auto n = 0; n < data.length; ++n) 
+    assert (a[n] == data[n]);
+
+  assert (a[data.length] == "o hai", 
+          "Incorrect value at insertion point.");
+}
+
+unittest {
+  writeln("Array: erasing a single item in the middle of the list.");
+  auto a = Array!int(iota(0, 100, 1));
+  assert(a.length == 100);
+  assert(a[7] == 7);
+
+  a.erase(7);    
+  assert(a.length == 99);
+  foreach(i; 0 .. 7)
+    assert(a[i] == i);
+
+  foreach(i; 7 .. 99)
+    assert(a[i] == (i+1));
 }
 
 version(unittest) {
-    /**
-     * A test struct that tracks the execution of its destructor, for use in
-     * testing.
-     */
-    struct DtorTestValue {
-        static int dtorCount = 0;
-        int n;
-        ~this() {
-            if (n != 0) {
-                ++dtorCount;                
-            }
-        }
+  /**
+   * A test struct that tracks the execution of its destructor, for use in
+   * testing.
+   */
+  struct DtorTestValue {
+    static int dtorCount = 0;
+    int n;
+    ~this() {
+      if (n != 0) {
+        ++dtorCount;                
+      }
     }
+  }
 }
 
 
 unittest {
-    // erasing the only item in an array 
-    auto a = Array!DtorTestValue();    
-    a.insertBack(DtorTestValue(1));
-    DtorTestValue.dtorCount = 0;
-    a.erase(0);   
-    assert(a.length == 0);    
-    assert (DtorTestValue.dtorCount == 1, 
-            "bad dtor count: " ~ to!string(DtorTestValue.dtorCount));
+  writeln("Array: erasing the only item in an Array.");
+  auto a = Array!DtorTestValue();    
+  a.insertBack(DtorTestValue(1));
+  DtorTestValue.dtorCount = 0;
+  a.erase(0);   
+  assert(a.length == 0);    
+  assert (DtorTestValue.dtorCount == 1, 
+          "bad dtor count: " ~ to!string(DtorTestValue.dtorCount));
 }
 
 unittest {
-    // erasing the first item in an array    
-    Array!DtorTestValue a;
-    a.reserve(100);
-    foreach(n; 0 .. 100)
-        a.insertBack(DtorTestValue(n+1));
+  writeln("Array: erasing the first item in an Array.");
+  Array!DtorTestValue a;
+  a.reserve(100);
+  foreach(n; 0 .. 100)
+    a.insertBack(DtorTestValue(n+1));
 
-    DtorTestValue.dtorCount = 0;
-    a.erase(0);    
-    assert(a.length == 99);
+  DtorTestValue.dtorCount = 0;
+  a.erase(0);    
+  assert(a.length == 99);
 
-    assert (DtorTestValue.dtorCount == 1, 
-            "bad dtor count: " ~ to!string(DtorTestValue.dtorCount));
+  assert (DtorTestValue.dtorCount == 1, 
+          "bad dtor count: " ~ to!string(DtorTestValue.dtorCount));
 
-    foreach(i; 0 .. 99) {
-        assert(a[i].n == (i+2), 
-              "Invalid post-erase value: expected " ~ to!string(i+2) ~ 
-              ", got: " ~ to!string(a[i].n));
-    }
+  foreach(i; 0 .. 99) {
+    assert(a[i].n == (i+2), 
+          "Invalid post-erase value: expected " ~ to!string(i+2) ~ 
+          ", got: " ~ to!string(a[i].n));
+  }
 }
 
 unittest {
-    // erasing the last item in an array
-    Array!DtorTestValue a;
-    a.reserve(100);
-    foreach(n; 0 .. 100)
-        a.insertBack(DtorTestValue(n+1));
+  writeln("Array: erasing the last item in an array.");
+  Array!DtorTestValue a;
+  a.reserve(100);
+  foreach(n; 0 .. 100)
+    a.insertBack(DtorTestValue(n+1));
 
-    DtorTestValue.dtorCount = 0;
-    a.erase(99);    
-    assert(a.length == 99);
+  DtorTestValue.dtorCount = 0;
+  a.erase(99);    
+  assert(a.length == 99);
 
-    assert (DtorTestValue.dtorCount == 1, 
-            "bad dtor count: " ~ to!string(DtorTestValue.dtorCount));
+  assert (DtorTestValue.dtorCount == 1, 
+          "bad dtor count: " ~ to!string(DtorTestValue.dtorCount));
 
-    foreach(i; 0 .. 99)
-        assert(a[i].n == i+1);
+  foreach(i; 0 .. 99)
+    assert(a[i].n == i+1);
 }
 
 unittest {
-    // erasing a single essentially random item
-    Array!DtorTestValue a;
-    a.reserve(100);
-    foreach(n; 0 .. 100)
-        a.insertBack(DtorTestValue(n+1));
+  writeln("Array: erasing a single, essentially random, item.");
+  Array!DtorTestValue a;
+  a.reserve(100);
+  foreach(n; 0 .. 100)
+    a.insertBack(DtorTestValue(n+1));
 
-    DtorTestValue.dtorCount = 0;
-    a.erase(7);    
-    assert(a.length == 99);
+  DtorTestValue.dtorCount = 0;
+  a.erase(7);    
+  assert(a.length == 99);
 
-    assert (DtorTestValue.dtorCount == 1, 
-            "bad dtor count: " ~ to!string(DtorTestValue.dtorCount));
+  assert (DtorTestValue.dtorCount == 1, 
+          "bad dtor count: " ~ to!string(DtorTestValue.dtorCount));
 
-    foreach(i; 0 .. 7)
-        assert(a[i].n == (i+1));
+  foreach(i; 0 .. 7)
+    assert(a[i].n == (i+1));
 
-    foreach(i; 7 .. 99)
-        assert(a[i].n == (i+2));
+  foreach(i; 7 .. 99)
+    assert(a[i].n == (i+2));
 }
 
 unittest {
-    // erasing a range of items from the array
-    auto a = Array!int(iota(0, 100, 1));
-    a.erase(25, 50);
-    assert (a.length == 50, "Post-erasure length is wrong: " ~ to!string(a.length)); 
+  writeln("Array: erasing a range of items from the array.");
+  auto a = Array!int(iota(0, 100, 1));
+  a.erase(25, 50);
+  assert (a.length == 50, "Post-erasure length is wrong: " ~ to!string(a.length)); 
 
-    foreach(i; 0 .. 25)
-        assert(a[i] == i, "Bad pre-erased-range value");
+  foreach(i; 0 .. 25)
+    assert(a[i] == i, "Bad pre-erased-range value");
 
-    foreach(i; 25 .. 50)
-        assert(a[i] == (i+50), "Bad post-erased-range value. " ~
-                               "Expected " ~ to!string(i+25) ~ 
-                               ", got " ~ to!string(a[i]));
+  foreach(i; 25 .. 50)
+    assert(a[i] == (i+50), "Bad post-erased-range value. " ~
+                           "Expected " ~ to!string(i+25) ~ 
+                           ", got " ~ to!string(a[i]));
 }
 
 unittest {
-    // erasing the entire array
-    auto a = Array!int(iota(0, 100, 1));
-    assert(a.length == 100);
-    a.erase(0, 100);
-    assert(a.length == 0, "Array should be empty after erasing it all.");
-    assert(a.capacity == 0, "Internal capacity should be 0 after erasing all items.");
+  writeln("Array: erasing the entire array");
+  auto a = Array!int(iota(0, 100, 1));
+  assert(a.length == 100);
+  a.erase(0, 100);
+  assert(a.length == 0, "Array should be empty after erasing it all.");
+  assert(a.capacity == 0, "Internal capacity should be 0 after erasing all items.");
 }
